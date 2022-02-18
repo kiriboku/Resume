eccc3693e1dd15ee41bb364e97d0378bb40109f1
kiriboku <n1076436@ntub.edu.tw>
2022-02-18 09:50:05 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 let menu = document.querySelector('#menu-btn')
 let header = document.querySelector('.header')
+const teslaWeb = document.getElementById('tesla-website')
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times')
     header.classList.toggle('active')
 }
 
 let themeToggler = document.querySelector('#theme-toggler')
 
-themeToggler.onclick = () =>{
+themeToggler.onclick = () => {
     themeToggler.classList.toggle('fa-sun')
-    if(themeToggler.classList.contains('fa-sun')){
+    if (themeToggler.classList.contains('fa-sun')) {
         document.body.classList.add('active')
-    }else{
+    } else {
         document.body.classList.remove('active')
     }
+}
+
+teslaWeb.onclick = () => {
+    window.open("./project/Tesla Website/index.html")
 }
